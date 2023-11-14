360a6707db374378f280784d8970210405639adf
LIU <98462984+LZMBill@users.noreply.github.com>
2023-11-14 17:38:21 +0000

    Add files via upload

--- Grid_world_lab/lab_search_1234567.pl
+++ Grid_world_lab/lab_search_1234567.pl
@@ -1,5 +1,6 @@
 
-% Perform a BFS to find the nearest oracle
+
+% BFS search
 search_bf :-
     my_agent(A),
     get_agent_position(A,P),
@@ -11,10 +12,29 @@ search_bf(Queue,Visited,Path) :-
     Queue = [Next|Rest],
     Next = [Pos|RPath],
     (complete(Pos) -> reverse([Pos|RPath],[_|Path])
-    ;otherwise     -> (findall([NPos,Pos|RPath],(map_adjacent(Pos,NPos,empty),\+ member(NPos,Visited), \+ member([NPos|_],Rest)), Newfound),
+    ;otherwise     -> findall([NPo000s,Pos|RPath],(map_adjacent(Pos,NPos,empty),\+ member(NPos,Visited), \+ member([NPos|_],Rest)), Newfound),
                       append(Rest,Newfound,NewQueue),
-                      search_bf(NewQueue,[Pos|Visited],Path))).
+                      search_bf(NewQueue,[Pos|Visited],Path)).
+                      
+% DFS search
+search_df :-
+    my_agent(A),
+    get_agent_position(A,StartPos),
+    search_df([StartPos],[],Path),
+    agent_do_moves(A,Path).
 
-    
+search_df(Current,Visited,Path) :-
+    Current = [Pos|RPath],
+    complete(Pos),
+    reverse([Pos|RPath],[_|Path]).
+
+search_df(Current,Visited,Path) :-
+    Current = [Pos|RPath],
+    \+ complete(Pos),
+    map_adjacent(Pos,NPos,empty),
+    \+ member(NPos,Visited),
+    search_df([NPos,Pos|RPath],[Pos|Visited],Path).
+
 % Test if the objective has been completed at a given position
 complete(Pos) :- map_adjacent(Pos,_,o(_)).
+
